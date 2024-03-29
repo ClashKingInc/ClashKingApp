98176afd0cf098d0155d0e7775feaeefe1bfddea
ValentineGilles <71833334+ValentineGilles@users.noreply.github.com>
2024-03-29 15:38:23 -0400

    ADD begining of player info page design

--- lib/subpages/player_dashboard/player_stats_page.dart
+++ lib/subpages/player_dashboard/player_stats_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clashkingapp/api/player_account_info.dart';
+import 'dart:ui';
+import 'package:clipboard/clipboard.dart';
 
 class StatsScreen extends StatefulWidget {
   final PlayerAccountInfo playerStats;
@@ -10,93 +12,182 @@ class StatsScreen extends StatefulWidget {
   StatsScreenState createState() => StatsScreenState();
 }
 
-class StatsScreenState extends State<StatsScreen> {
-  String dropdownValue = 'Home Base';
+class StatsScreenState extends State<StatsScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Player Stats'),
-      ),
-      body: ListView(
+      body: Column(
         children: [
-          ListTile(
-            title: Text('Name: ${widget.playerStats.name}'),
-            subtitle: Text('Tag: ${widget.playerStats.tag}'),
+          Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              SizedBox(
+                height: 180,
+                child: ImageFiltered(
+                  imageFilter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+                  child: ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.3), // Adjust opacity as needed
+                      BlendMode.darken,
+                    ),
+                    child: Image.network(
+                      "https://clashkingfiles.b-cdn.net/landscape/home-landscape.png",
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: -70,
+                child:
+                    Image.network(widget.playerStats.townHallPic, width: 170),
+              ),
+              Positioned(
+                top: 10,
+                left: 10,
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back,
+                      color: Theme.of(context).colorScheme.onPrimary, size: 32),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ),
+            ],
           ),
+          SizedBox(height: 64),
           ListTile(
-            title: DropdownButton<String>(
-              value: dropdownValue,
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownValue = newValue!;
-                });
-              },
-              items: <String>['Home Base', 'Builder Base', 'Clan Capital']
-                .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                })
-                .toList(),
+            title: Center(
+              child: Text(
+                widget.playerStats.name,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            subtitle: Center(
+              child: InkWell(
+                onTap: () {
+                  FlutterClipboard.copy(widget.playerStats.tag).then((value) {
+                    final snackBar = SnackBar(
+                      content: Text('Copied to Clipboard'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(8.0), // Add padding if needed
+                  child: Text(widget.playerStats.tag),
+                ),
+              ),
             ),
           ),
-          if (dropdownValue == 'Home Base') ...[
-            ListTile(
-              title: Text('Town Hall Level: ${widget.playerStats.townHallLevel}'),
+          TabBar(
+            controller: tabController,
+            tabs: [Tab(text: 'Home Base'), Tab(text: 'Builder Base')],
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: tabController,
+              children: [
+                // Home Base tab
+                ListView(
+                  children: [
+                    ListTile(
+                      title: Text(
+                          'Town Hall Level: ${widget.playerStats.townHallLevel}'),
+                    ),
+                    ListTile(
+                      title: Text('Trophies: ${widget.playerStats.trophies}'),
+                    ),
+                    ExpansionTile(
+                      title: Text('Heroes'),
+                      children: widget.playerStats.heroes
+                          .where((hero) => hero.village == 'home')
+                          .map((hero) => ListTile(
+                                title: Text(hero.name),
+                                subtitle: Text(
+                                    'Level: ${hero.level} / ${hero.maxLevel}'),
+                              ))
+                          .toList(),
+                    ),
+                    ExpansionTile(
+                      title: Text('Troops'),
+                      children: widget.playerStats.troops
+                          .where((troop) =>
+                              troop.village == 'home' &&
+                              !troop.name.startsWith('Super'))
+                          .map((troop) => ListTile(
+                                title: Text(troop.name),
+                                subtitle: Text(
+                                    'Level: ${troop.level} / ${troop.maxLevel} - ${troop.village}'),
+                              ))
+                          .toList(),
+                    ),
+                    ExpansionTile(
+                      title: Text('Spells'),
+                      children: widget.playerStats.spells
+                          .map((spell) => ListTile(
+                                title: Text(spell.name),
+                                subtitle: Text(
+                                    'Level: ${spell.level} / ${spell.maxLevel} - ${spell.village}'),
+                              ))
+                          .toList(),
+                    ),
+                  ],
+                ),
+
+                // Builder Base tab
+                ListView(
+                  children: [
+                    ListTile(
+                      title: Text(
+                          'Builder Hall Level: ${widget.playerStats.builderHallLevel}'),
+                    ),
+                    ListTile(
+                      title: Text(
+                          'Builder Base Trophies: ${widget.playerStats.builderBaseTrophies}'),
+                    ),
+                    ExpansionTile(
+                      title: Text('Heroes'),
+                      children: widget.playerStats.heroes
+                          .where((hero) => hero.village == 'builderBase')
+                          .map((hero) => ListTile(
+                                title: Text(hero.name),
+                                subtitle: Text(
+                                    'Level: ${hero.level} / ${hero.maxLevel}'),
+                              ))
+                          .toList(),
+                    ),
+                    ExpansionTile(
+                      title: Text('Troops'),
+                      children: widget.playerStats.troops
+                          .where((troop) => troop.village == 'builderBase')
+                          .map((troop) => ListTile(
+                                title: Text(troop.name),
+                                subtitle: Text(
+                                    'Level: ${troop.level} / ${troop.maxLevel} - ${troop.village}'),
+                              ))
+                          .toList(),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            ListTile(
-              title: Text('Trophies: ${widget.playerStats.trophies}'),
-            ),
-            ExpansionTile(
-              title: Text('Heroes'),
-              children: widget.playerStats.heroes.where((hero) => hero.village == 'home').map((hero) => ListTile(
-                title: Text(hero.name),
-                subtitle: Text('Level: ${hero.level} / ${hero.maxLevel}'),
-              )).toList(),
-            ),
-            ExpansionTile(
-              title: Text('Troops'),
-              children: widget.playerStats.troops.where((troop) => troop.village == 'home' && !troop.name.startsWith('Super')).map((troop) => ListTile(
-                title: Text(troop.name),
-                subtitle: Text('Level: ${troop.level} / ${troop.maxLevel} - ${troop.village}'),
-              )).toList(),
-            ),
-            ExpansionTile(
-              title: Text('Spells'),
-              children: widget.playerStats.spells.map((spell) => ListTile(
-                title: Text(spell.name),
-                subtitle: Text('Level: ${spell.level} / ${spell.maxLevel} - ${spell.village}'),
-              )).toList(),
-            ),
-          ]
-          else if (dropdownValue == 'Builder Base') ...[
-            ListTile(
-              title: Text('Builder Hall Level: ${widget.playerStats.builderHallLevel}'),
-            ),
-            ListTile(
-              title: Text('Builder Base Trophies: ${widget.playerStats.builderBaseTrophies}'),
-            ),
-            ExpansionTile(
-              title: Text('Heroes'),
-              children: widget.playerStats.heroes.where((hero) => hero.village == 'builderBase').map((hero) => ListTile(
-                title: Text(hero.name),
-                subtitle: Text('Level: ${hero.level} / ${hero.maxLevel}'),
-              )).toList(),
-            ),
-            ExpansionTile(
-              title: Text('Troops'),
-              children: widget.playerStats.troops.where((troop) => troop.village == 'builderBase').map((troop) => ListTile(
-                title: Text(troop.name),
-                subtitle: Text('Level: ${troop.level} / ${troop.maxLevel} - ${troop.village}'),
-              )).toList(),
-            ),
-          ]
-          else ...[
-            ListTile(title: Text('No data'),)
-          ]
+          ),
         ],
       ),
     );
