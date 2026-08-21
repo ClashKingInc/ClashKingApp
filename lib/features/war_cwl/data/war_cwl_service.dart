8c6e9f784a945e368d97058965bbcb6760824af6
Destinea <valentine-gilles@orange.fr>
2026-08-21 23:25:33 +0200

    fix: keep clans without current war

--- lib/features/war_cwl/data/war_cwl_service.dart
+++ lib/features/war_cwl/data/war_cwl_service.dart
@@ -221,7 +221,8 @@ WarCwl? _parseWarSummary(dynamic item, [Set<String>? requestedTags]) {
         (requestedTags != null && !requestedTags.contains(tag))) {
       return null;
     }
-    if (json['war_info'] is! Map ||
+    final warInfo = json['war_info'];
+    if ((warInfo != null && warInfo is! Map) ||
         (json['war_league_infos'] != null &&
             json['war_league_infos'] is! List)) {
       return null;
